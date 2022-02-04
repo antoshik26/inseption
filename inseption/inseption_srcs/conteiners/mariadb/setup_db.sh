01d4e565b6d93cb770843817f01b2d01eb775fea
antoshik26 <antoshik_26@mail.ru>
2022-02-04 13:45:53 +0300

    sdfsssssfds

--- inseption/inseption_srcs/conteiners/mariadb/setup_db.sh
+++ inseption/inseption_srcs/conteiners/mariadb/setup_db.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-servise mysql start
+service mysql start
 mysql -e "CREATE USER '${MARIA_LOGIN}'@'localhost' identified by '${MARIA_PASS}';" &&\
 mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_LOGIN}'@'%' IDENTIFIED BY '${MARIA_PASS}';" &&\
--- inseption/inseption_srcs/conteiners/wordpress/wp_config.php
+++ inseption/inseption_srcs/conteiners/wordpress/wp_config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root1' );
 define( 'DB_PASSWORD', '123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost:3306' );
+define( 'DB_HOST', 'mariadb_service:3306' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
