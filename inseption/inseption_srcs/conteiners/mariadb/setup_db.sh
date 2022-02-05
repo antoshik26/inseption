3a129ffc83918c9a94895be3d4be6870fb8feab5
root <antoshik_26@mail.ru>
2022-02-05 20:13:15 +0300

    asb

--- inseption/inseption_srcs/conteiners/mariadb/setup_db.sh
+++ inseption/inseption_srcs/conteiners/mariadb/setup_db.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 
 service mysql start
-mysql -e "CREATE USER 'dmadelei'@'localhost' identified by '11';" &&\
 mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'dmadelei'@'%' IDENTIFIED BY '11';" &&\
+mysql -e "CREATE USER IF NOT EXISTS 'dmadelei'@'localhost' IDENTIFIED by 'password';" &&\
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'dmadelei'@'localhost' IDENTIFIED BY 'password';" &&\
+mysql -e "CREATE USER IF NOT EXISTS 'dmadelei'@'%' IDENTIFIED by 'password';" &&\
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'dmadelei'@'%' IDENTIFIED BY 'password';" &&\
 mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root password ${DB_PASS}
 service mysql stop
 
 mysqld
--- inseption/inseption_srcs/conteiners/wordpress/setup.sh
+++ inseption/inseption_srcs/conteiners/wordpress/setup.sh
@@ -1,8 +1,9 @@
-#!bin/bash/
-
-chown -R www-data:www-data /var/www/wordpress/
-
-sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
-mkdir -p /run/php/
-
-exec /usr/sbin/php-fpm7.3 -F
+if [ ! -f /var/www/wordpress/wp_config.php ]; then
+	wp core --allow-root download --locate=ru_RU --force --path='/var/www/wordpress'
+	while [! -f /var/www/wordpress/wp_config.php]; do
+		wp core config --allow-root --db_name=wordpress --path='/var/www/wordpress' --dbuser=$DB_LOGIN --dbpass=$DB_PASS --dbhost=mariadb
+	done
+	wp core install --allow-root --url='dmadelei.42.fr' --title='WordPress for Inception' --admin_user=$WP_LOGIN --admin_password=$WP_PASS --admin_email="admin@admin.com" --path='var/www/wordpress';
+	wp user create --allow-root $WPU_1LOGIN user1@user.com --user_pass=$WPU_1PASS --role=author --path='/var/www/wordpress/';
+fi
+php-fpm7.3 --nodaemonize
