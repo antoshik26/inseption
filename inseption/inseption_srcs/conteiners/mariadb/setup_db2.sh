0d88729467575a39c16050fbd2bba7f95f670725
root <antoshik_26@mail.ru>
2022-02-06 18:45:05 +0300

    inseption and pacages

--- inseption/inseption_srcs/conteiners/mariadb/setup_db2.sh
+++ inseption/inseption_srcs/conteiners/mariadb/setup_db2.sh
@@ -1,4 +1,5 @@
-if [ ! -d "var/mysql/lib/wordpress/" ]; then
+if [ ! -d "/var/mysql/lib/wordpress/" ]; then
+	mkdir -p /var/mysql/lib/wordpress/
 	service mysql start
 	mysql -u root < tmp.sql	
 	mysqladmin -u root password 11
--- inseption/inseption_srcs/conteiners/wordpress/setup.sh
+++ inseption/inseption_srcs/conteiners/wordpress/setup.sh
@@ -1,9 +1,7 @@
 if [ ! -f /var/www/wordpress/wp_config.php ]; then
 	wp core --allow-root download --locale=ru_RU --force --path='/var/www/wordpress'
-	while [ ! -f /var/www/wordpress/wp_config.php ]; do
-		wp core config --allow-root --dbname=wordpress --path='/var/www/wordpress' --dbuser=dmadelei --dbpass=password --dbhost=mariadb
-	done
-	wp core install --allow-root --url='dmadelei.42.fr' --title='WordPress for Inception' --admin_user=dmadelei --admin_password=11 --admin_email="admin@admin.com" --path='/var/www/wordpress';
+		wp core config --allow-root --dbname=wordpress --path='/var/www/wordpress' --dbuser=dmadelei --dbpass=11 --dbhost=mariadb
+	wp core install --allow-root --url='dmadelei.42.fr' --title='WordPress for Inception' --admin_user=root --admin_password=11 --admin_email="admin@admin.com" --path='/var/www/wordpress';
 	wp user create --allow-root $WPU_1LOGIN user1@user.com --user_pass=$WPU_1PASS --role=author --path='/var/www/wordpress/';
 fi
 php-fpm7.3 --nodaemonize
